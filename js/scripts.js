c650b08525dfd895c7610ba5074018e06e4b34f3
HarrisonStrand <harrisonstrand@gmail.com>
2020-11-19 18:25:55 -0800

    add newPizza.price functionality after prototype and declaration fixes! add clean html with simple styling and refactoring

--- js/scripts.js
+++ js/scripts.js
@@ -15,34 +15,36 @@ Customer.prototype.allInfo = function () {
 function Pizza (toppings, size) {
   this.toppings = toppings;
   this.size = size;
-}
+  this.price = 0;
+};
 
 Pizza.prototype.pizzaPrice = function () {
-  let price = this.toppings.length;
-
+  let addCost = this.toppings.length;
   if (this.size === "small") {
-    price += 10;
+    addCost += 10;
   } else if (this.size === "medium") {
-    price += 14;
+    addCost += 14;
   } else {
-    price += 20;
+    addCost += 20;
   }
-  return price;
+
+  this.price = addCost;
+
 };
 
 //UI LOGIC
 $(document).ready(function() {
-  
   $("button#checkout").click(function(event) {
     event.preventDefault();
-  
+    
     $("#order-form").each(function() {
+      let price;
       let toppings = $('input:checkbox:checked').map(function() {
         return this.value;
       });
       let size = $('input:radio:checked').val();
       let newPizza = new Pizza(toppings, size);
-
+      newPizza.pizzaPrice(price);
       let name = $(this).find("#user-name").val();
       let phone = $(this).find("#user-phone").val();
       let address = $(this).find("#user-address").val();
@@ -50,7 +52,7 @@ $(document).ready(function() {
       
       $("#finalCustomerPhone").text( "Phone Number: " + (newCustomer.phone));
       $("#finalCustomerAddress").text( "Address: " + (newCustomer.address));
-      $("#finalCustomerPrice").text((newCustomer.name) + ", your total is: " + "$" + (newPizza.pizzaPrice()));
+      $("#finalCustomerPrice").text((newCustomer.name) + ", your total is: " + "$" + (newPizza.price));
     });
   });
 });
